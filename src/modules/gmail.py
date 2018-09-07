b261dd2cd336a43be2632ac23d06709455082b55
Zultan <43057246+ZultanH@users.noreply.github.com>
2018-09-07 17:48:36 +1000

    Create gmail.py

---
+++ src/modules/gmail.py
@@ -0,0 +1,26 @@
+import smtplib
+
+def send_email(user, password, recipient, subject, body):
+    gmail_user = user
+    gmail_pwd = password
+    FROM = user
+    TO = recipient if type(recipient) is list else [recipient]
+    SUBJECT = subject
+    TEXT = body
+
+    # Prepare actual message
+    message = """From: %s\nTo: %s\nSubject: %s\n\n%s
+    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.ehlo()
+    server.starttls()
+    server.login(gmail_user, gmail_pwd)
+    server.sendmail(FROM, TO, message)
+    server.close()
+
+def send_product_token(recipient, token):
+    username = ""
+    password = ""
+
+    send_email(username, password, recipient, "GROWTH Product Token", f"--PRODUCT TOKEN FOR GROWTH SERVICES--\n Token: {token}" )
